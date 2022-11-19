883f8497ae92b0978188ec115b54a5cd1d00e4c6
jelenkoo10 <veljkojelenkovic00@gmail.com>
2022-11-19 13:07:18 +0100

    Adding another page and structuring files

--- js/single-ad.js
+++ js/single-ad.js
@@ -1,37 +1,3 @@
-const hamburger = document.getElementById("hamburger")
-const nav = document.getElementById("nav")
-const modalBtn = document.querySelector(".modal-btn")
-const cancelBtn = document.querySelector(".cancel-btn")
-const overlay = document.getElementById("overlay")
-
-if (window.innerWidth < 700) {
-    nav.style.display = "none"
-} else {
-    nav.style.display = "block"
-}
-
-hamburger.addEventListener("click", function() {
-    if (nav.style.display == "none") {
-        nav.style.display = "block"
-    } else {
-        nav.style.display = "none"
-    }
-})
-
-modalBtn.addEventListener("click", function() {
-    if (overlay.style.display == "none") {
-        overlay.style.display = "flex"
-    } else {
-        overlay.style.display = "none"
-    }
-})
-
-cancelBtn.addEventListener("click", function() {
-    overlay.style.display = "none"
-})
-
-/////////////////////////////////
-/* Single ad page */
 const togglerLeft = document.querySelector(".toggler-one")
 const togglerRight = document.querySelector(".toggler-two")
 const adImage = document.querySelector(".ad-image")
